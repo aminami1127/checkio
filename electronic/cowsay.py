d0cab76f2bbb7cea5a81cc78232844f37dda9d07
aminami1127 <aminami1127@users.noreply.github.com>
2015-08-17 15:34:46 +0900

    clear cow

--- electronic/cowsay.py
+++ electronic/cowsay.py
@@ -13,40 +13,47 @@ MAX_ROW = 39
 
 
 def make_rows(text):
-    rows, i = [''], 0
+    rows, num = [''], 0
     words = text.split(' ')
-    for w in words:
-        if len(rows[i]) + len(w) >= MAX_ROW:
-            i += 1
+    for i, w in enumerate(words):
+        # slice words that have over MAX_ROW characters
+        if len(w) > MAX_ROW:
+            words.insert(words.index(w) + 1, w[MAX_ROW:])
+            w = w[:MAX_ROW]
+        # if the word is first, don't append row
+        if len(rows[num]) + len(w) >= MAX_ROW and i:
+            num += 1
             rows.append('')
-        rows[i] += w if not rows[i] else ' ' + w
+        rows[num] += w if not rows[num] else ' ' + w
     return rows
 
 
 def cowsay(text):
+    import re
+    text = re.sub('\s+', ' ', text)
     if len(text) <= MAX_ROW:
         quote = r'''
  {}
 < {} >
  {}'''
         width = len(text) + 2
-        return quote.format(TOP * width, text, BOTTOM * width) + COW
     else:
         quote = r'''
  {}
 {}
  {}'''
-        width = MAX_ROW + 2
         rows = make_rows(text)
+        width = max(len(x) for x in rows) + 2
+        concat = lambda x, y, z: x + ' ' + y + ' ' + z
         for i, row in enumerate(rows):
             if i == 0:
-                rows[i] = BORDERS[0] + ' ' + rows[i].ljust(MAX_ROW) + ' ' + BORDERS[1]
+                rows[i] = concat(BORDERS[0], rows[i].ljust(width - 2), BORDERS[1])
             elif i == len(rows) - 1:
-                rows[i] = BORDERS[1] + ' ' + rows[i].ljust(MAX_ROW) + ' ' + BORDERS[0]
+                rows[i] = concat(BORDERS[1], rows[i].ljust(width - 2), BORDERS[0])
             else:
-                rows[i] = BORDERS[2] + ' ' + rows[i].ljust(MAX_ROW) + ' ' + BORDERS[2]
+                rows[i] = concat(BORDERS[2], rows[i].ljust(width - 2), BORDERS[2])
         text = '\n'.join(rows)
-        return quote.format(TOP * width, text, BOTTOM * width) + COW
+    return quote.format(TOP * width, text, BOTTOM * width) + COW
 
 
 if __name__ == '__main__':
@@ -88,11 +95,10 @@ if __name__ == '__main__':
 '''
 
     cowsay_one_line = cowsay('Checkio rulezz')
-    #assert cowsay_one_line == expected_cowsay_one_line, 'Wrong answer:\n%s' % cowsay_one_line
+    assert cowsay_one_line == expected_cowsay_one_line, 'Wrong answer:\n%s' % cowsay_one_line
 
     cowsay_two_lines = cowsay('A longtextwithonlyonespacetofittwolines.')
-    import pdb;pdb.set_trace()
     assert cowsay_two_lines == expected_cowsay_two_lines, 'Wrong answer:\n%s' % cowsay_two_lines
 
     cowsay_many_lines = cowsay('Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do ' 'eiusmod tempor incididunt ut labore et dolore magna aliqua.')
-    # assert cowsay_many_lines == expected_cowsay_many_lines, 'Wrong answer:\n%s' % cowsay_many_lines
+    assert cowsay_many_lines == expected_cowsay_many_lines, 'Wrong answer:\n%s' % cowsay_many_lines
