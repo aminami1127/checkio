199cbe3f2967de6a2f8d9a8d322a9f8462a1f8bb
aminami1127 <aminami1127@users.noreply.github.com>
2015-09-03 12:09:01 +0900

    mod

--- practice/generator/ex9.py
+++ practice/generator/ex9.py
@@ -3,13 +3,13 @@
 def send_value_generator():
     i = (yield)
     while True:
-        i = (yield i)
+        i = (yield i ** 2)
 
 
 sample = send_value_generator()
 next(sample)
-print(sample.send('first'))
-print(sample.send('second'))
-print(sample.send('third'))
-print(sample.send('fourth'))
-print(sample.send('fifith'))
+print(sample.send(1))
+print(sample.send(2))
+print(sample.send(3))
+print(sample.send(4))
+print(sample.send(5))
