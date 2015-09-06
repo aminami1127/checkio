56dce2a7904f79295d1a502e9a4d57ab7c04259f
aminami1127 <aminami1127@users.noreply.github.com>
2015-09-06 13:53:44 +0900

    mod

--- ice/shoot.py
+++ ice/shoot.py
@@ -21,13 +21,13 @@ def shot(wall1, wall2, shot_point, later_point):
     direction = lambda crd1, crd2: tuple((x[0] - x[1]) for x in zip(crd1, crd2))
     distance = lambda crd1, crd2: math.sqrt(sum((x[0] - x[1]) ** 2 for x in zip(crd1, crd2)))
 
-    center = tuple(avg(c1, c2) for c1, c2 in zip(wall1, wall2))
     ballistic, wall = direction(later_point, shot_point), direction(wall2, wall1)
     hit_point, k = calc_hitpoint(shot_point, ballistic, wall1, wall)
 
     if k and k > 0 and all(
         sorted(w)[0] <= hit_point[i] <= sorted(w)[1] for i, w in enumerate(zip(wall1, wall2))
     ):
+        center = tuple(avg(c1, c2) for c1, c2 in zip(wall1, wall2))
         score = calc_equation((0, 100), (distance(wall1, wall2) / 2, 0))
         return round(score(distance(center, hit_point)))
     else:
