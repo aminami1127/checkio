5fa7335d41dc16c5859abc909388f96ed2ee58a4
aminami1127 <aminami1127@users.noreply.github.com>
2015-09-13 16:11:38 +0900

    clear pattern

---
+++ mine/pattern.py
@@ -0,0 +1,56 @@
+import itertools as it
+
+
+def checkio(pattern, image):
+    pattern_height, pattern_width = len(pattern), len(list(zip(*pattern)))
+    image_height, image_width = len(image), len(list(zip(*image)))
+    scan = lambda: it.product(range(pattern_height), range(pattern_width))
+
+    for i, j in it.product(range(image_height), range(image_width)):
+        try:
+            if all(image[i + x][j + y] == pattern[x][y] for (x, y) in scan()):
+                for x, y in scan():
+                    image[i + x][j + y] += 2
+        except IndexError:
+            continue
+    return image
+
+
+#These "asserts" using only for self-checking and not necessary for auto-testing
+if __name__ == '__main__':
+    assert checkio([[1, 0], [1, 1]],
+                   [[0, 1, 0, 1, 0],
+                    [0, 1, 1, 0, 0],
+                    [1, 0, 1, 1, 0],
+                    [1, 1, 0, 1, 1],
+                    [0, 1, 1, 0, 0]]) == [[0, 3, 2, 1, 0],
+                                          [0, 3, 3, 0, 0],
+                                          [3, 2, 1, 3, 2],
+                                          [3, 3, 0, 3, 3],
+                                          [0, 1, 1, 0, 0]]
+    assert checkio([[1, 1], [1, 1]],
+                   [[1, 1, 1],
+                    [1, 1, 1],
+                    [1, 1, 1]]) == [[3, 3, 1],
+                                    [3, 3, 1],
+                                    [1, 1, 1]]
+    assert checkio([[0, 1, 0], [1, 1, 1]],
+                   [[0, 0, 1, 0, 0, 0, 0, 0, 1, 0],
+                    [0, 1, 1, 1, 0, 0, 0, 1, 1, 1],
+                    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+                    [0, 1, 0, 0, 1, 1, 1, 0, 1, 0],
+                    [1, 1, 1, 0, 0, 0, 0, 0, 1, 1],
+                    [0, 0, 0, 1, 1, 1, 0, 0, 0, 0],
+                    [0, 0, 1, 0, 0, 0, 0, 1, 0, 0],
+                    [0, 1, 1, 0, 0, 0, 1, 1, 1, 0],
+                    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]) == [[0, 2, 3, 2, 0, 0, 0, 2, 3, 2],
+                                                         [0, 3, 3, 3, 0, 0, 0, 3, 3, 3],
+                                                         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                                                         [0, 0, 0, 0, 2, 3, 2, 0, 0, 0],
+                                                         [2, 3, 2, 0, 3, 3, 3, 0, 1, 0],
+                                                         [3, 3, 3, 0, 0, 0, 0, 0, 1, 1],
+                                                         [0, 0, 0, 1, 1, 1, 0, 0, 0, 0],
+                                                         [0, 0, 1, 0, 0, 0, 2, 3, 2, 0],
+                                                         [0, 1, 1, 0, 0, 0, 3, 3, 3, 0],
+                                                         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
