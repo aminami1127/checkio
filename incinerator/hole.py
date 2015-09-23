de5f86f5f4f654ee1131e6f9711ca44a290c37e3
aminami1127 <aminami1127@users.noreply.github.com>
2015-09-23 20:01:11 +0900

    clear rotate

---
+++ incinerator/hole.py
@@ -0,0 +1,16 @@
+def rotate(state, pipe_numbers):
+    result = []
+    rotate = lambda L: [L[len(L) - 1]] + L[:len(L) - 1]
+    for i in range(len(state)):
+        if all(state[x] for x in pipe_numbers):
+            result.append(i)
+        state = rotate(state)
+    return result
+
+
+if __name__ == '__main__':
+    #These "asserts" using only for self-checking and not necessary for auto-testing
+    assert rotate([1, 0, 0, 0, 1, 1, 0, 1, 0, 0, 0, 1], [0, 1]) == [1, 8], "Example"
+    assert rotate([1, 0, 0, 0, 1, 1, 0, 1, 0, 0, 0, 1], [0, 1, 2]) == [], "Mission impossible"
+    assert rotate([1, 0, 0, 0, 1, 1, 0, 1], [0, 4, 5]) == [0], "Don't touch it"
+    assert rotate([1, 0, 0, 0, 1, 1, 0, 1], [5, 4, 5]) == [0, 5], "Two cannonballs in the same pipe"
