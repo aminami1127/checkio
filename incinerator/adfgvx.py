4805a9214dab062c150705dea141f4dc27b1e882
aminami1127 <aminami1127@users.noreply.github.com>
2015-10-08 23:12:56 +0900

    in progress

--- incinerator/adfgvx.py
+++ incinerator/adfgvx.py
@@ -2,6 +2,7 @@ SYMBOLS = 'ADFGVX'
 
 
 def divide(length, word):
+    """divide the word by length size"""
     i = 0
     for _ in range(length):
         sliced = word[i:i+length]
@@ -12,6 +13,7 @@ def divide(length, word):
 
 
 def uniqualize(keyword):
+    """remove duplicate alphabet of keyword(order isn't changed)"""
     uniqued_key = ''
     for c in keyword:
         if c not in uniqued_key:
@@ -19,46 +21,60 @@ def uniqualize(keyword):
     return uniqued_key
 
 
+def convert_square(secret_alphabet):
+    return [x for x in divide(len(SYMBOLS), secret_alphabet)]
+
+
 def encode(message, secret_alphabet, keyword):
     clean = lambda x: ''.join(y for y in x.lower() if y.isalnum())
+    convert = convert_square(secret_alphabet)
 
-    matrix = [x for x in divide(len(SYMBOLS), secret_alphabet)]
+    # convert message by secret_alphabet square
     converted = ''.join(
-        ''.join(SYMBOLS[i] + SYMBOLS[row.index(c)] for i, row in enumerate(matrix) if c in row)
+        ''.join(
+            SYMBOLS[i] + SYMBOLS[row.index(c)]
+            for i, row in enumerate(convert) if c in row
+        )
         for c in clean(message)
     )
+
+    # create matrix to encode message
     uniqued_key = uniqualize(keyword)
-    new_matrix = [x for x in divide(len(uniqued_key), converted)]
+    matrix = [x for x in divide(len(uniqued_key), converted)]
     encoded = ''.join(
         ''.join(y for y in x[1] if y.isalpha()) for x in
-        sorted(zip(uniqued_key, zip(*new_matrix)), key=lambda x: x[0])
+        sorted(zip(uniqued_key, zip(*matrix)), key=lambda x: x[0])
     )
     return encoded
 
 
 def decode(message, secret_alphabet, keyword):
     uniqued_key = uniqualize(keyword)
-    length = len(uniqued_key)
-    key_contains_size = zip(
+    # num of the characters that each key alphabet related
+    size_of_keyalphas = zip(
         uniqued_key,
-        [len([y for y in x if y.isalpha()]) for x in zip(*divide(length, message))]
+        [len([y for y in x if y.isalpha()]) for x in zip(*divide(len(uniqued_key), message))]
     )
-    i, matrix = 0, []
-    for k, size in sorted(key_contains_size, key=lambda x: x[0]):
-        matrix.append(message[i:i+size])
+
+    # create matrix to decode message
+    i, sorted_matrix = 0, []
+    for k, size in sorted(size_of_keyalphas, key=lambda x: x[0]):
+        sorted_matrix.append(message[i:i+size])
         i += size
-    tmp = list(
-        x[1].ljust(3) for x in
-        sorted(zip(sorted(uniqued_key), matrix), key=lambda x: keyword.index(x[0]))
+    matrix = list(
+        x[1].ljust(len(sorted_matrix)) for x in
+        sorted(zip(sorted(uniqued_key), sorted_matrix), key=lambda x: keyword.index(x[0]))
     )
-    tmp = ''.join(''.join(x) for x in zip(*tmp)).strip()
+    message = ''.join(''.join(x) for x in zip(*matrix)).strip()
+
+    # decode each pair characters of the message by convert_square
     decoded = ''
-    matrix = [x for x in divide(len(SYMBOLS), secret_alphabet)]
-    iter_tmp = zip(tmp, tmp[1:])
-    for i, j in iter_tmp:
-        decoded += matrix[SYMBOLS.index(i)][SYMBOLS.index(j)]
+    convert = convert_square(secret_alphabet)
+    iter_message = zip(message, message[1:])
+    for i, j in iter_message:
+        decoded += convert[SYMBOLS.index(i)][SYMBOLS.index(j)]
         try:
-            next(iter_tmp)
+            next(iter_message)
         except:
             break
     return decoded
@@ -71,21 +87,21 @@ if __name__ == '__main__':
     assert decode("FXGAFVXXAXDDDXGA",
                   "dhxmu4p3j6aoibzv9w1n70qkfslyc8tr5e2g",
                   "cipher") == 'iamgoing', "decode I am going"
-    # assert encode("attack at 12:00 am",
-    #               "na1c3h8tb2ome5wrpd4f6g7i9j0kjqsuvxyz",
-    #               "privacy") == 'DGDDDAGDDGAFADDFDADVDVFAADVX', "encode attack"
-    # assert decode("DGDDDAGDDGAFADDFDADVDVFAADVX",
-    #               "na1c3h8tb2ome5wrpd4f6g7i9j0kjqsuvxyz",
-    #               "privacy") == 'attackat1200am', "decode attack"
-    # assert encode("ditiszeergeheim",
-    #               "na1c3h8tb2ome5wrpd4f6g7i9j0kjqsuvxyz",
-    #               "piloten") == 'DFGGXXAAXGAFXGAFXXXGFFXFADDXGA', "encode ditiszeergeheim"
-    # assert decode("DFGGXXAAXGAFXGAFXXXGFFXFADDXGA",
-    #               "na1c3h8tb2ome5wrpd4f6g7i9j0kjqsuvxyz",
-    #               "piloten") == 'ditiszeergeheim', "decode ditiszeergeheim"
-    # assert encode("I am going",
-    #               "dhxmu4p3j6aoibzv9w1n70qkfslyc8tr5e2g",
-    #               "weasel") == 'DXGAXAAXXVDDFGFX', "encode weasel == weasl"
-    # assert decode("DXGAXAAXXVDDFGFX",
-    #               "dhxmu4p3j6aoibzv9w1n70qkfslyc8tr5e2g",
-    #               "weasel") == 'iamgoing', "decode weasel == weasl"
+    assert encode("attack at 12:00 am",
+                  "na1c3h8tb2ome5wrpd4f6g7i9j0kjqsuvxyz",
+                  "privacy") == 'DGDDDAGDDGAFADDFDADVDVFAADVX', "encode attack"
+    assert decode("DGDDDAGDDGAFADDFDADVDVFAADVX",
+                  "na1c3h8tb2ome5wrpd4f6g7i9j0kjqsuvxyz",
+                  "privacy") == 'attackat1200am', "decode attack"
+    assert encode("ditiszeergeheim",
+                  "na1c3h8tb2ome5wrpd4f6g7i9j0kjqsuvxyz",
+                  "piloten") == 'DFGGXXAAXGAFXGAFXXXGFFXFADDXGA', "encode ditiszeergeheim"
+    assert decode("DFGGXXAAXGAFXGAFXXXGFFXFADDXGA",
+                  "na1c3h8tb2ome5wrpd4f6g7i9j0kjqsuvxyz",
+                  "piloten") == 'ditiszeergeheim', "decode ditiszeergeheim"
+    assert encode("I am going",
+                  "dhxmu4p3j6aoibzv9w1n70qkfslyc8tr5e2g",
+                  "weasel") == 'DXGAXAAXXVDDFGFX', "encode weasel == weasl"
+    assert decode("DXGAXAAXXVDDFGFX",
+                  "dhxmu4p3j6aoibzv9w1n70qkfslyc8tr5e2g",
+                  "weasel") == 'iamgoing', "decode weasel == weasl"
